0ff52ab2004d099d8a1aa3110da5b7ff8ed50e35
Naillton <nailton_junior@protonmail.com>
2023-10-21 00:19:04 -0300

    Add animation elements motion

---
+++ app/src/main/java/com/example/animatestate/BoxPosition.kt
@@ -0,0 +1,5 @@
+package com.example.animatestate
+
+enum class BoxPosition {
+    Start, End
+}
--- app/src/main/java/com/example/animatestate/MainActivity.kt
+++ app/src/main/java/com/example/animatestate/MainActivity.kt
@@ -5,15 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -29,8 +36,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.animatestate.ui.theme.AnimateStateTheme
 
@@ -45,7 +54,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     // RotationDemo()
-                    ColorChangeDemo()
+                    // ColorChangeDemo()
+                    MotionDemo()
                 }
             }
         }
@@ -53,7 +63,7 @@ class MainActivity : ComponentActivity() {
 }
 
 /**
- * Projeto criado para criar estado de animacao state driven
+ * Projeto criado para criar estado de animacao state driven, animate dp as state
  */
 
 @Composable
@@ -129,11 +139,71 @@ private fun ColorChangeDemo() {
     }
 }
 
+// Animacao na mudanca de posicao de um elemento
+@Composable
+private fun MotionDemo(){
+
+    var positionState by remember { mutableStateOf(BoxPosition.Start) }
+    val boxSideLenght = 70.dp
+
+    // obtendo tamanho da tela do dispositivo atraves do localConfiguration
+    val screenWidth = (LocalConfiguration.current.screenWidthDp.dp)
+
+    val animatedPosition: Dp by animateDpAsState(
+        // definindo target value para mudarmos o elemento da posicao inicial x ate a posicao final y
+        targetValue = when (positionState) {
+            BoxPosition.Start -> 0.dp
+            BoxPosition.End -> screenWidth - boxSideLenght
+        },
+        // animacao com o spring
+        // animationSpec = spring(dampingRatio = DampingRatioHighBouncy)
+        // animationSpec = tween(4500),
+        // animacao com o spring usando stiffness
+        // animationSpec = spring(dampingRatio = Spring.DampingRatioHighBouncy, stiffness = Spring.StiffnessVeryLow),
+        // animacao usando keyframes
+        animationSpec = keyframes {
+            durationMillis = 1000
+            100.dp.at(10)
+            110.dp.at(500)
+            200.dp.at(700)
+            },
+        label = ""
+    )
+
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .offset(x = animatedPosition)
+                .size(boxSideLenght)
+                .background(Color.Red)
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Button(
+            onClick = {
+                positionState = when (positionState) {
+                    BoxPosition.Start -> BoxPosition.End
+                    BoxPosition.End -> BoxPosition.Start
+                }
+            },
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(text = "Change Position")
+        }
+
+    }
+
+}
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     AnimateStateTheme {
         // RotationDemo()
-        ColorChangeDemo()
+        // ColorChangeDemo()
+        MotionDemo()
     }
 }
